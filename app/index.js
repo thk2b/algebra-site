03311277361bdb5c5d1065212de5f88a7d9120a2
Theo <theokobb@gmail.com>
2018-04-28 00:08:57 -0400

    Implement basic app

--- app/index.js
+++ app/index.js
@@ -1,8 +1,27 @@
 import { calculate, simplify } from '@thk2b/algebra';
 
-console.log(
-    calculate('1+1')   
-)
-console.log(
-    calculate('1+1')   
-)
+const $expression = document.getElementById('expression');
+const $operation = document.getElementById('operation');
+const $run = document.getElementById('run');
+const $result = document.getElementById('result');
+
+const operations = {
+    calculate, simplify
+};
+
+const state = {
+    operation: 'calculate',
+    expression: ''
+};
+
+$operation.addEventListener('change', e => {
+    state.operation = $operation.value;
+});
+
+$expression.addEventListener('change', e => {
+    state.expression = $expression.value;
+});
+
+$run.addEventListener('click', e => {
+    $result.innerHTML = operations[state.operation](state.expression);
+});
